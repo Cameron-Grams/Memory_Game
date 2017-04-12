24d6a2af0535e245e3201b5d0859ec915a8a63e6
Cameron <cameron.grams.2@gmail.com>
2017-04-11 23:06:05 -0400

    first commit

---
+++ src/Cell.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Cell extends React.Component {
+  active(){
+    return this.props.activeCells.indexOf(this.props.id) >= 0;
+  }
+
+
+  render(){
+    let className = 'cell';
+    if (this.props.gameState === "memorize" && this.active()){
+      className += " active";
+    }
+
+
+    return (
+      <div className={className}>
+      </div>
+    );
+  }
+}
+
+export default Cell;
